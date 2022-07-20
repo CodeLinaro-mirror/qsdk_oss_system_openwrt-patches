8973299d3c7dc7793c5a40e8651fd7188410aa0d
Suruchi Suman <quic_surusuma@quicinc.com>
2022-07-20 13:10:06 +0530

    ipq95xx: enable MAPT client driver
    Change-Id: I99982671fff2b32ca20d9fdfb9ba3e1ed6660f77
    Signed-off-by: Suruchi Suman <quic_surusuma@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -38,7 +38,8 @@ NSS_PPE:= kmod-qca-nss-ppe \
 	kmod-qca-nss-ppe-lag-mgr \
 	kmod-qca-nss-ppe-tunipip6 \
 	kmod-qca-nss-ppe-gretap \
-	kmod-qca-nss-ppe-vxlanmgr
+	kmod-qca-nss-ppe-vxlanmgr \
+	kmod-qca-nss-ppe-mapt
 
 NSS_CLIENTS_STANDARD:= kmod-qca-nss-drv-qdisc kmod-qca-nss-drv-igs kmod-qca-nss-drv-tun6rd \
 	kmod-qca-nss-drv-tunipip6 kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-pptp \
