6d5affaea08f9c3fa7f4a0745967b8a25bfd38ec
Anurag Sethi <quic_anurseth@quicinc.com>
2022-11-21 23:51:59 -0800

    qca-rsrcmgr: Build qca-rsrcmgr for Enterprise and 512MB profiles
    Change-Id: I5af205f821b1b524fa3dc675300b524b58cc524c
    Signed-off-by: Anurag Sethi <quic_anurseth@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -317,7 +317,7 @@ define Profile/QSDK_Enterprise
 		$(IPSEC) $(NSS_EIP197_FW) $(CD_ROUTER) $(AQ_PHY) \
 		$(CNSS_DIAG) $(CTRL_APP_DUT) $(FTM) $(QMSCT_CLIENT) -lacpd \
 		$(USB_DIAG) $(MHI_QRTR) $(KPI) $(FAILSAFE) $(NSS_USERSPACE) \
-		$(NSS_PPE) kmod-qca-nss-drv-mscs
+		$(NSS_PPE) kmod-qca-nss-drv-mscs $(RSRC_MGR)
 endef
 
 define Profile/QSDK_Enterprise/Description
@@ -335,7 +335,8 @@ define Profile/QSDK_MinEnt
 		$(QCA_ECM_ENTERPRISE) $(NSS_CLIENTS_ENTERPRISE) $(NSS_MACSEC) $(NSS_CRYPTO) \
 		$(IPSEC) $(NSS_EIP197_FW) $(CD_ROUTER) $(AQ_PHY) $(CNSS_DIAG) \
 		$(CTRL_APP_DUT) $(FTM) $(QMSCT_CLIENT) -lacpd -kmod-qca-nss-ecm-wifi-plugin \
-		$(USB_DIAG) $(MHI_QRTR) $(KPI) $(FAILSAFE) $(NSS_USERSPACE) $(NSS_PPE)
+		$(USB_DIAG) $(MHI_QRTR) $(KPI) $(FAILSAFE) $(NSS_USERSPACE) $(NSS_PPE) \
+		$(RSRC_MGR)
 endef
 
 define Profile/QSDK_MinEnt/Description
@@ -374,7 +375,7 @@ define Profile/QSDK_512
 		$(COREBSP_UTILS) $(MAP_PKGS) $(AQ_PHY) $(FAILSAFE) -lacpd $(USB_DIAG) \
 		$(NSS_EIP197_FW) $(CNSS_DIAG) $(CTRL_APP_DUT) $(FTM) $(QMSCT_CLIENT) $(KPI) \
 		$(NSS_USERSPACE) $(NSS_RMNET) $(HYFI) kmod-qca-hyfi-bridge $(EMESH_SP) \
-		$(QCA_EZMESH) kmod-macvlan $(MINIDUMP)
+		$(QCA_EZMESH) kmod-macvlan $(MINIDUMP) $(RSRC_MGR)
 endef
 
 #       $(MHI_QRTR)
