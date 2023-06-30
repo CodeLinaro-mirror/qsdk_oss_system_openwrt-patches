2212194570d725fee1f22feb3f67c48a4b3b8b0e
Kannan Saravanan <quic_kannsara@quicinc.com>
2023-06-30 10:31:38 +0530

    WIFI_PKG: Add myftm package to the 256LM Profile
    Adding myftm package to the 256LM Profile.
    
    Change-Id: I24babd25b5e78c3bb3c19cb8155b39ed6b4a0f07
    Signed-off-by: Kannan Saravanan <quic_kannsara@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -118,7 +118,7 @@ WIFI_PKGS_256MB:=kmod-qca-wifi-lowmem-profile \
 	qca-wpa-cli qca-cfg80211tool qca-wifi-scripts \
 	qca-wpc sigma-dut qca-wrapd qca-wapid qca-acfg \
 	qca-iface-mgr qca-icm qca-cfg80211 athdiag qca-cnss-daemon \
-	athtestcmd-lith whc-mesh whc-ui
+	athtestcmd-lith whc-mesh whc-ui myftm
 
 WIFI_PKGS_16M:=kmod-qca-wifi-flash_16mb-profile \
 	qca-hostap qca-hostapd-cli qca-wpa-supplicant \
