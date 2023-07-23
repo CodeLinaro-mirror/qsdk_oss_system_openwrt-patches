f907f65fe2ba6580c97b8c66aaca6ce3b6235c8e
Nagarajan Maran <quic_nmaran@quicinc.com>
2023-07-24 01:47:20 +0530

    [qsdk] Add PPE DS support for open profile.
    With the latest changes in ATH driver, the PPE DS support needs
    to be enabled in qsdk.mk in-order to get it built.
    
    Fixes: b6ab84872662 ("ath11k-ath12k: integration and compilation fixes with openwrt-Ginger")
    Change-Id: Ic39b2420916aba28622231d1fd2839bd8ba86fe3
    Signed-off-by: Nagarajan Maran <quic_nmaran@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -304,7 +304,7 @@ define Profile/QSDK_Open
 		qca-cnss-daemon qca-wifi-hk-fw-hw1-10.4-asic $(CNSS_DIAG) athdiag $(EMESH_SP) \
 		qrtr $(QMI_SAMPLE_APP) ath11k-fwtest ath11k-qdss -qapp-store \
 		libtirpc cfr_tools kmod-qca-ovsmgr -qca-mcs-apps -kmod-qca-nss-ecm-wifi-plugin \
-		kmod-macvlan myftm
+		kmod-macvlan myftm kmod-qca-nss-ppe-ds
 endef
 
 #	$(HW_CRYPTO) $(QCA_RFS)
