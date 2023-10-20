91f7e91ce3d39a554db6338b413df345bd22846b
Tian Yang <quic_tiany@quicinc.com>
2023-10-20 10:40:25 -0700

    qsdk.mk Disable ifli for premium/bigendian profile
    ifli will be depending on tensorflow which will be disabled
    in premium/bigendian profile due to space limitation.
    
    Change-Id: Icac52e9892918b32587cdb56c76eeedf025de606
    Signed-off-by: Tian Yang <quic_tiany@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -273,7 +273,7 @@ define Profile/QSDK_Premium
 		$(QOS) $(HYFI) $(NSS_MACSEC) $(NSS_USERSPACE) $(NSS_USERSPACE_OSS) $(NSS_RMNET) \
 		$(NSS_UDP_ST) $(NSS_PPE) $(QCA_MAD) $(EMESH_SP) $(NETSTANDBY) \
 		$(QCA_EZMESH) $(OPENVPN) kmod-macvlan kmod-qca-hyfi-bridge kmod-netstandby-eawtp-drv \
-		$(NSS_NSM) $(SAL_QOS) $(RSRC_MGR) $(NSS_FLOWID) $(NSS_FLS) $(NPT66)
+		$(NSS_NSM) $(SAL_QOS) $(RSRC_MGR) $(NSS_FLS) $(NPT66)
 endef
 
 #		$(QCA_RFS)
@@ -297,7 +297,7 @@ define Profile/QSDK_BigEndian
                 $(QOS) $(HYFI) $(NSS_MACSEC) $(NSS_USERSPACE) $(NSS_USERSPACE_OSS) $(NSS_RMNET) \
                 $(NSS_UDP_ST) $(NSS_PPE) $(QCA_MAD) $(EMESH_SP) \
                 $(QCA_EZMESH) $(OPENVPN) kmod-macvlan kmod-qca-hyfi-bridge \
-                $(NSS_NSM) $(SAL_QOS) $(RSRC_MGR) $(NSS_FLOWID) $(NSS_FLS) $(NPT66)
+                $(NSS_NSM) $(SAL_QOS) $(RSRC_MGR) $(NSS_FLS) $(NPT66)
 endef
 
 #               $(QCA_RFS)
