a904755b6994b38df4e6b1e8423f4ba97453130d
Harish Rachakonda <quic_rachakon@quicinc.com>
2024-08-19 17:15:48 +0530

    wifi-scripts.mk: Copied the mac80211.sh and hostapd.sh
    Currently the scripts are placed inside the open-wrt/patches
    directory, which is copied using this Makefile.
    
    Change-Id: Ib62fdd004e412dcc246fb22d2b94137f8d5d09f8
    Signed-off-by: Harish Rachakonda <quic_rachakon@quicinc.com>
    (cherry picked from commit 4a12efadfb7c68e26d4021dab2395c28f7fdacc0)

--- package/network/config/wifi-scripts/wifi-scripts.mk
+++ package/network/config/wifi-scripts/wifi-scripts.mk
@@ -1,9 +1,18 @@
 # Recipe extension for package : wifi-scripts
 
+EXTERNAL_FILE_DIR:=$(TOPDIR)/openwrt-patches/package/kernel/mac80211
+FILE_DIR_HOSTAPD:=$(TOPDIR)/openwrt-patches/package/network/services/hostapd/
+
 ifeq ($(CONFIG_PACKAGE_kmod-ath11k)$(CONFIG_PACKAGE_kmod-ath12k),)
 define wifi-scripts_install_append
 	rm -rf $(1)/lib/wifi/mac80211.sh
 	rm -rf $(1)/lib/netifd/hostapd.sh
+endef
+else
+define wifi-scripts_install_append
+	$(INSTALL_DATA) $(EXTERNAL_FILE_DIR)/files/lib/wifi/mac80211.sh $(1)/lib/wifi
+	$(INSTALL_BIN) $(EXTERNAL_FILE_DIR)/files/lib/netifd-wlan/wireless/mac80211.sh $(1)/lib/netifd/wireless/
+	$(INSTALL_DATA) $(FILE_DIR_HOSTAPD)/files/hostapd.sh $(1)/lib/netifd/hostapd.sh
 endef
 endif
 
