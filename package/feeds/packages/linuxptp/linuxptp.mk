be295ba941e45653a0197465a129ea56704f714b
Luo Jie <luoj@qti.qualcomm.com>
2026-04-10 15:32:48 +0800

    linuxptp: add init script and firewall support for PTP daemons
    Add procd init script and firewall rules to start ptp4l, ts2phc and
    phc2sys on bootup for ipq5210/ipq9650 boards.
    
    Change-Id: Iad37c0a6d32a7fe43cbab5ea67438cac108c6965
    Signed-off-by: Luo Jie <luoj@qti.qualcomm.com>

---
+++ package/feeds/packages/linuxptp/linuxptp.mk
@@ -0,0 +1,18 @@
+# Recipe extension for package : linuxptp
+
+LINUXPTP:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+define linuxptp_install_append
+	$(INSTALL_DIR) $(1)/etc/init.d
+	$(INSTALL_DIR) $(1)/etc/config
+	$(INSTALL_DIR) $(1)/etc/firewall.d
+	$(INSTALL_DIR) $(1)/etc/uci-defaults
+	$(INSTALL_DIR) $(1)/usr/sbin
+	$(INSTALL_DATA) $(LINUXPTP)/files/ptp.uci.config $(1)/etc/config/ptp
+	$(INSTALL_BIN) $(LINUXPTP)/files/ptp.init $(1)/etc/init.d/ptp
+	$(INSTALL_BIN) $(LINUXPTP)/files/ptp-sequencer $(1)/usr/sbin/ptp-sequencer
+	$(INSTALL_BIN) $(LINUXPTP)/files/ptp.firewall $(1)/etc/firewall.d/ptp
+	$(INSTALL_DATA) $(LINUXPTP)/files/ptp.defaults $(1)/etc/uci-defaults/99-ptp
+endef
+
+Package/linuxptp/install += $(newline)$(linuxptp_install_append)
